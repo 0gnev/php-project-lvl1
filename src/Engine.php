00ecf5b81eaa7e1f10e5a0b54a2cbc3839834209
0gnev <j.kodanavi@gmail.com>
2020-06-10 16:24:33 +0700

    delete Cli, all in Engine

--- src/Engine.php
+++ src/Engine.php
@@ -2,34 +2,27 @@
 
 namespace BrainGames\Engine;
 
-use BrainGames\Cli;
-
+use function cli\prompt;
 use function cli\line;
 
-function compare($answer, $correctAnswer, $name)
-{
-    if ($answer == $correctAnswer) {
-        line("Correct!");
-        return true;
-    } else {
-        line(
-            '"%s" is wrong answer ;(. Correct answer was "%s". Let\'s try again, %s!',
-            $answer,
-            $correctAnswer,
-            $name
-        );
-        return false;
-    }
-}
 function play($intro, $gameData)
 {
-    $name = Cli\introduce($intro);
-    $count = 0;
-    while ($count < count($gameData)) {
-        $answer = Cli\ask($gameData[$count]['question']);
-        if (compare($answer, $gameData[$count]['answer'], $name)) {
-            $count++;
+    line("Welcome to Brain Games!");
+    line($intro);
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    foreach ($gameData as $round) {
+        line("Question: %s", $round['question']);
+        $answer = prompt("Your answer");
+        if ($answer == $round['answer']) {
+            line("Correct!");
         } else {
+            line(
+                '"%s" is wrong answer ;(. Correct answer was "%s". Let\'s try again, %s!',
+                $answer,
+                $round['answer'],
+                $name
+            );
             return;
         }
     }
